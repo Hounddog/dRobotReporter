ce7b5d8850993801688c5206c86fc941ed9383f6
Martin Shwalbe <mshwalbe@doyousoft.com>
2012-07-30 12:44:03 +0200

    Corrected connection plugin

--- connector.js
+++ connector.js
@@ -1,42 +1,32 @@
-define(["doh/runner", "dojo/_base/xhr", "dojo/_base/connect","dojo/json", "dojo/io-query"], function(doh, xhr, connect, json, ioQuery) {
-    var uri = window.location.search;
+define([
+	"doh/runner", 
+	"dojo/_base/connect",
+	"dojo/io-query"
+	], function(doh,  connect,  ioQuery) {
     
+    var uri = window.location.search;
+    console.log('uri', uri)
     var query = uri.substring(uri.indexOf("?") + 1, uri.length);
     var queryObject = ioQuery.queryToObject(query);
-    
-    var data = json.stringify({'doh': 'start', "browser": queryObject.browser});
-    xhr.post({
-	    url: "/receiver.php",
-	    sync:true,
-	    content:{
-			'COMMAND': data
-	    }
-	});
-	
-    connect.connect(doh, "debug", function(){
-		var data = json.stringify({'message':Array.prototype.join.call(arguments, " "), "browser": queryObject.browser })
-		xhr.post({
-		    url: "/receiver.php",
-		    sync:true,
-		    content:{
-			'DATA': data
-		    }, 
-		    load:function(data) {
-			window.close();
-		    }
-		})
-    });
-    
+
+	var socket = io.connect(queryObject.server + ':' + queryObject.port + '/');
+
+    connect.connect(doh, "debug", function() {
+    	var data = {
+    		"message": Array.prototype.join.call(arguments, " "),
+    		"browser": queryObject.browser 
+    	}
+    	socket.emit('debug', data);
+    	console.log('send node message');
+    }); 
+
     connect.connect(doh, "_report", function(){
-		var data = json.stringify({'doh':'result', 'failureCount': doh._failureCount, 'errorCount': doh._errorCount})
-		xhr.post({
-		    url: "/receiver.php",
-		    sync:true,
-		    content:{
-				'COMMAND': data
-		    }, 
-		    load:function(data) {
-		    }
-		})
+    	var data = {
+    		"failureCount": doh._failureCount,
+    		"errorCount": doh._errorCount,
+    		"browser": queryObject.browser 
+    	}
+    	socket.emit('result', data);
+    	console.log('send result');
     });
 });
